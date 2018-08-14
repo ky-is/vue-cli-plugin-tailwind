e18dd8a7a328c4c46c142527e62146237bc1b1cd
Kyle Coburn <kylecoburn@gmail.com>
2018-08-14 15:13:13 -0700

    ✨ Install devDependencies

--- generator/index.js
+++ generator/index.js
@@ -1,2 +1,9 @@
 module.exports = (api, options) => {
+	api.extendPackage({
+		devDependencies: {
+			"@fullhuman/postcss-purgecss": "^1.0.1",
+			"postcss-import": "^12.0.0",
+			"tailwindcss": "^0.6.5",
+		},
+	})
 }
