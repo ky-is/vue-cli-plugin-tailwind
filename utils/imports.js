9dc5a17d685d55aa9dcc4d6394e66bd1a82b694c
Kyle Coburn <kylecoburn@gmail.com>
2018-08-14 15:15:43 -0700

    🌱 Imports helper

---
+++ utils/imports.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+	addToFileAt (filePath, importPath) {
+		const importStatement = `import '${importPath}'`
+		const fs = require('fs')
+		const mainFileString = fs.readFileSync(filePath, 'utf-8')
+		if (mainFileString.indexOf(importStatement) !== -1) {
+			return
+		}
+		const lines = mainFileString.split(/\r?\n/g)
+		let lastImportLineIndex = 0
+		for (let index = 1; index < lines.length; index += 1) {
+			const line = lines[index].trim()
+			if (line.length) {
+				if (!line.startsWith('import ')) {
+					break
+				}
+				lastImportLineIndex = index
+			}
+		}
+		lines[lastImportLineIndex] += `\n\n${importStatement}`
+		fs.writeFileSync(filePath, lines.join('\n'), 'utf-8')
+	},
+
+}
