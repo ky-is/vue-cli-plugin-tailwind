e792526e2007bdc76c17f28529345853dd0d3968
Kyle Coburn <kylecoburn@gmail.com>
2018-08-15 15:19:50 -0700

    🏗 Include Purgecss in development
    Catch any incorrectly purged classes before production

--- generator/templates/postcss.config.js
+++ generator/templates/postcss.config.js
@@ -1,13 +1,11 @@
-const isDev = process.argv.includes('serve')
-
 module.exports = {
   plugins: [
-    require('postcss-import'),
+    require('postcss-import')(),
     require('postcss-preset-env')({ stage: 2 }),
     require('tailwindcss')('./tailwind.config.js'),
-    isDev ? null : require('@fullhuman/postcss-purgecss')({
+    require('@fullhuman/postcss-purgecss')({
       content: [ './src/**/*.vue', './src/**/*.@(css|less|sass|scss|styl)' ],
     }),
-    require('autoprefixer'),
+    require('autoprefixer')(),
   ],
 }
