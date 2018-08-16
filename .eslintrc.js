84239098625214dc34b84839c72165dda84e1dac
Kyle Coburn <kylecoburn@gmail.com>
2018-08-15 18:52:10 -0700

    ➕ @ky-is/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,7 @@
 module.exports = {
 	root: true,
-	env: {
-		node: true,
-	},
-	parserOptions: {
-		ecmaVersion: 2015,
-	},
-	rules: {
-		'comma-dangle': [ 'error', 'always-multiline' ],
-		'curly': 'error',
-		'indent': [ 'error', 'tab' ],
-		'no-multiple-empty-lines': [ 'error', {
-			max: 1,
-		}],
-		'no-var': 'error',
-	},
+
+	extends: [
+		'@ky-is',
+	],
 }
