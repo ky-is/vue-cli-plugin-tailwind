4767d80a2c12a7f66c403de06d83112d33d5de96
Kyle Coburn <kylecoburn@gmail.com>
2019-04-27 23:55:52 -0700

    🐛 Regression: extracting blank files

--- generator/templates/postcss.config.js
+++ generator/templates/postcss.config.js
@@ -8,7 +8,7 @@ module.exports = {
       content: [ `./public/**/*.html`, `./src/**/*.vue` ],
       defaultExtractor (content) {
         const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
-        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g)
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
       },
       whitelist: [],
       whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
